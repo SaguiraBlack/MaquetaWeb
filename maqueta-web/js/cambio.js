b8d0a9f73d9ea85f18f5e27d374c5b6bdae71f81
Josefina Tovar <39281486+SaguiraBlack@users.noreply.github.com>
2021-01-29 15:16:11 -0600

    Add files via upload

--- maqueta-web/js/cambio.js
+++ maqueta-web/js/cambio.js
@@ -1,4 +1,40 @@
 function cambio(id){
-    //let elemento1 = document.getElementById("")
-    alert(id);
+    let elemento1 = document.getElementById("img")
+    elemento1.src = "img/cel"+(id+1)+".jpg"
+    titulo = document.getElementById("titulo")
+    marca = document.getElementById("marca")
+    precio = document.getElementById("precio")
+    titulo.innerHTML = celulares[id].titulo
+    marca.innerHTML = "Marca: "+celulares[id].marca
+    precio.innerHTML = "$"+celulares[id].precio.toFixed(2)+" MXN"
+}
+
+celulares =[{
+    titulo: "Xiaomi Redmi Note",
+    marca: "Xiaomi",
+    precio: 4689
+},
+{
+    titulo: "Xiaomi Redmi 9",
+    marca: "Xiaomi",
+    precio: 4098
+},
+{
+    titulo: "Smartphone Redmi Note",
+    marca: "Xiaomi",
+    precio: 3749
+},
+{
+    titulo: "Samsung Galaxy A21s",
+    marca: "Samsung",
+    precio: 4747
+},
+{
+    titulo: "Samsung Galaxy S8",
+    marca: "Samsung",
+    precio: 6999
+}];
+
+function gracias(){
+    alert("Gracias por comprar :3 ")
 }
